022d34ecc2eafb2c559e2ce61660398ba0b36f5e
vagrant <vagrant@precise64.(none)>
2017-02-09 23:36:15 +0000

    compl

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int start;
+
+	for (start = 'a'; start <= 'z'; start++)
+	{
+		if (start == 'e')
+		{
+		continue;
+		}
+		if (start == 'q')
+		{
+		continue;
+		}
+		putchar(start);
+	}
+	putchar('\n');
+	return (0);
+}
