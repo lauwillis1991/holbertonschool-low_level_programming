18b05e378d4bfd45fab484bd0c14df67a7fe9443
vagrant <vagrant@precise64.(none)>
2017-02-14 22:12:14 +0000

    lowerc

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _islower - Return 1 if letter is lowercase, 0 if uppercase.
+ * @c: char type letter inputted
+ * Return: 1 if lowercase, 0 if not lowercase
+ */
+
+int _islower(int c)
+
+{
+	if (c > 96 && c < 123)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
