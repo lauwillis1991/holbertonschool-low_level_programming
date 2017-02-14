c0e7c4d857d663d508d19d08e5f6dc0eb0720fb1
vagrant <vagrant@precise64.(none)>
2017-02-14 23:33:00 +0000

    commited

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - prints every minute of this guy.
+ * Description: starts from 00:00 to 23:59
+ */
+
+
+void jack_bauer(void)
+{
+	int counter = 0;
+	int a = 0, b = 0, c = 0, d = 0;
+
+	while (counter < 1440)
+	{
+		_putchar(a + '0');
+		_putchar(b + '0');
+		_putchar (':');
+		_putchar(c + '0');
+		_putchar(d + '0');
+		d++;
+		if (d > 9)
+		{
+			d = 0;
+			c++;
+		}
+		if (c > 5)
+		{
+			c =  0;
+			b++;
+		}
+		if (b > 9)
+		{
+			b = 0;
+			a++;
+		}
+		counter++;
+	}
+}
