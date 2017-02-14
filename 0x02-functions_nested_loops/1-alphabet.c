3bc96d38a1201199ad28283ed3de86eb5bbfc8db
vagrant <vagrant@precise64.(none)>
2017-02-14 21:33:57 +0000

    2nd problem

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - Print alphabet in lowercase
+ *
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
