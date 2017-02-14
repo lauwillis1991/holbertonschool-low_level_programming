c0da7d836283a88fb9889a968d31a323d8dfe186
vagrant <vagrant@precise64.(none)>
2017-02-14 23:07:31 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - print last digit of a number
+ * @n: int type number
+ * Return: return value of last digit
+ */
+
+int print_last_digit(int n)
+
+{
+	int m;
+
+	if (n < 0)
+	{
+	m = -1 * (n % 10);
+	putchar (m + '0');
+	return (m);
+	}
+	else
+	{
+		m = n % 10;
+		putchar (m + '0');
+		return (m);
+	}
+}
