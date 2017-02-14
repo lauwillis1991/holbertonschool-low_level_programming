abc5ea16e9daca63e4f4be07799107ac52ba44bf
vagrant <vagrant@precise64.(none)>
2017-02-14 21:00:24 +0000

    add header

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,7 +1,21 @@
-#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - print Holberton followed by newline
+ * Return: 0
+ */
 
 int main(void)
 {
-	puts("Holberton");
+	char holberton[] = "Holberton";
+	int i = 0;
+
+	while (holberton[i] != '\0')
+	{
+		_putchar(holberton[i]);
+		i++;
+	}
+	_putchar('\n');
+
 	return (0);
 }
