66a1f8cc78787bb1c4f7620cb7594618e9a23668
vagrant <vagrant@precise64.(none)>
2017-02-14 23:58:58 +0000

    added

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - Adding two integers
+ * @x: int type number
+ * @y: int type number
+ * Return: result
+ */
+
+int add (int x, int y)
+{
+	return (x + y);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * times_table - Print the 9 times table, starting with 0
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	
+}
