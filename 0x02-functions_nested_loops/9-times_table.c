a17122dc30ba8be40c937b3a2481437e87a00b19
vagrant <vagrant@precise64.(none)>
2017-02-15 04:45:01 +0000

    added 9 finally

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,5 +7,31 @@
 
 void times_table(void)
 {
-	
+	int i, j, result;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			result = i * j;
+			if (result < 10)
+			{
+				if (j > 0)
+				{
+					_putchar(',');
+					_putchar(" ");
+					_putchar(" ");
+				}
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
