85549436879b9ab0667aac529ae736110815ff17
vagrant <vagrant@precise64.(none)>
2017-02-15 00:16:41 +0000

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - Print all natural numbers from n to 98
+ * @n: int type number
+ */
+
+void print_to_98(int n)
+
+{
+	if (n <= 98)
+	{
+		for ( ; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else
+	{
+		for ( ; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d, ", n);
+
+}
