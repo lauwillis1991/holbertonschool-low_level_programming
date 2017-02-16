9eb538307a59ed9372a46ed5dec550c97020f4a8
vagrant <vagrant@precise64.(none)>
2017-02-16 21:43:22 +0000

    fixed line

--- 0x03-more_functions_nested_loops/6-print_line.c
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -12,16 +12,9 @@ void print_line(int n)
 
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		if(n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
-		{
 		_putchar('_');
-		}
 	}
 	_putchar('\n');
 }
