c40a306e027cd1e806bb3f499e2e227218c94fe9
vagrant <vagrant@precise64.(none)>
2017-02-16 20:35:25 +0000

    added stuff

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two parameters in the function.
+ * @a: type int character
+ * @b: type int character
+ * Return: multiplication of int a and b
+ */
+
+int mul(int a, int b)
+
+{
+	return (a * b);
+}
