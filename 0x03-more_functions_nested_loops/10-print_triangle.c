db7ac9b22ae357c4d733faafe52c8be348c703f4
vagrant <vagrant@precise64.(none)>
2017-02-16 23:50:33 +0000

    triangled

---
+++ 0x03-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: the size of the triangle
+ *
+ * Return: void.
+ */
+
+void print_triangle(int size)
+{
+
+int i, j;
+
+for (i = 0; i < size; i++)
+{
+	for (j = 0; j < size; j++)
+	{
+		if (j + 1 < size - i)
+		{
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar('#');
+		}
+	}
+	if (i != size)
+	{
+		_putchar('\n');
+	}
+}
+
+}
