3eaf46aca0adb6dbb3f92ea83812084341ea5c63
vagrant <vagrant@precise64.(none)>
2017-02-21 21:43:59 +0000

    found length

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of the string
+ * @s: char type pointer.
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; i[s] != '\0'; i++)
+
+		;
+	return (i);
+}
