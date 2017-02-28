d21b14f65351ea11592815d06cfab93e84447ad5
vagrant <vagrant@precise64.(none)>
2017-02-28 01:52:32 +0000

    added

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - Search a string for any of a set of bytes.
+ * @s: string
+ * @accept: string to detect
+ * Return: Pointer to the byte in s
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+	char *p;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (accept[j] == s[i])
+			{
+				p = &s[i];
+				return (p);
+			}
+			j++;
+		}
+		i++;
+	}
+
+	return (0);
+}
