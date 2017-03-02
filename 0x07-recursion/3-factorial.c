2ae97cbc715f120c8d22548a3b537469574ee633
vagrant <vagrant@precise64.(none)>
2017-03-02 01:00:16 +0000

    pushed

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * factorial - return factorial of parameter.
+ * @n: int type
+ * Return: factorial of n
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n < 2)
+		return (n);
+	else
+		return (n * factorial(n - 1));
+}
