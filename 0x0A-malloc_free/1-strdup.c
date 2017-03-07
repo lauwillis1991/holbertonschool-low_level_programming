259dd8f242a5ee044d0ee60903193c6ff0cdbb2c
vagrant <vagrant@precise64.(none)>
2017-03-07 22:56:27 +0000

    committed

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strdup - returns a pointer to newly allocated space in memory.
+ * @str: the string to get copied.
+ * Return: Null if str = NULL and return the copied string if success.
+ */
+
+char *_strdup(char *str)
+{
+	char *arr;
+	int i, j;
+
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		;
+		i++;
+	}
+	arr = malloc(i * sizeof(*arr));
+		if (arr == NULL)
+		{
+			return (NULL);
+		}
+	for (j = 0; j < i; j++)
+	{
+		arr[j] = str[j];
+	}
+
+	arr[j] = '\0';
+	return (arr);
+}
