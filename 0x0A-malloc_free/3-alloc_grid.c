1e377e394ee3241344777de2073e55d767668e9e
vagrant <vagrant@precise64.(none)>
2017-03-08 04:49:57 +0000

    sighed

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -27,7 +27,7 @@ int **alloc_grid(int width, int height)
 		grid[i] = malloc(width * sizeof(**grid));
 		if (grid[i] == NULL)
 		{
-			for (; i >= 0; i--)
+			for (i--; i >= 0; i--)
 			{
 				free(grid[i]);
 			}
