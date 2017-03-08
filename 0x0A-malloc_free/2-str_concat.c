021d2ced22aadb30f11daae281a42bfca43ce28e
vagrant <vagrant@precise64.(none)>
2017-03-08 03:22:09 +0000

    freed up some space

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -18,26 +18,19 @@ char *str_concat(char *s1, char *s2)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
 	for (i = 0; s1[i] != '\0'; i++)
 		;
-
-
 	for (j = 0; s2[j] != '\0'; j++)
 		;
-
 	arr = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
-
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
-
 	for (k = 0, m = 0; k < (i + j) + 1; k++)
 	{
 		if (k < i)
