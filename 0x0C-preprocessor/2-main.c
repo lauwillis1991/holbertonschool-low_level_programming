70492a43fd36f7b1bfe070cf1b19e48783f5999d
vagrant <vagrant@precise64.(none)>
2017-03-14 01:27:50 +0000

    headers added

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the file that it's compiling from
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0C-preprocessor/3-function_like_macro.h
@@ -0,0 +1,4 @@
+#ifndef ABSOLUT_H
+#define ABSOLUT_H
+#define ABS(x) ((x < 0) ? (x * -1) : x)
+#endif
