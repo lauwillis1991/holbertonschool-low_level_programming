8146dbf317285fb20bdf84d573534ade3cb6f8c6
vagrant <vagrant@precise64.(none)>
2017-03-15 18:36:42 +0000

    looping

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - goes through an array
+ * @array: pointer to array
+ * @size: size
+ * @action: function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
