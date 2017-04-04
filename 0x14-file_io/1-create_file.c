be4acc1e8f2bf812fa901367360bc4560cde93cb
vagrant <vagrant@precise64.(none)>
2017-04-04 23:46:55 +0000

    fixed loop

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, check, i = 0;
+	int fd, check, i;
 
 	if (filename == NULL)
 	{
@@ -25,15 +25,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while (text_content[i] != '\0')
-		{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
 		check = write(fd, text_content, i);
 		if (check == -1)
 			return (-1);
-		i++;
-		}
 	}
-
 	close(fd);
-	return (1);
+	return(1);
 }
